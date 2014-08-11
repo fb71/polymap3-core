e22d0edf050dd2fa0bfca71875b19c3c0f18f98c
falko <falko@localhost>
2014-08-11 14:08:20 +0200

    #76: Felder in Attributtabelle (http://polymap.org/biotop/ticket/76)   - FIX: chrome asks for favicon when opening report page - which ends session without this check

--- plugins/org.polymap.core/src/org/polymap/core/WorkbenchRedirectServlet.java
+++ plugins/org.polymap.core/src/org/polymap/core/WorkbenchRedirectServlet.java
@@ -40,7 +40,16 @@ public class WorkbenchRedirectServlet
     @Override
     protected void service( HttpServletRequest req, HttpServletResponse resp )
             throws ServletException, IOException {
-        log.info( "REDIRECT: request=" + req.getPathInfo() );
+        String pathInfo = req.getPathInfo();
+        log.info( "REDIRECT: request=" + pathInfo );
+
+        // XXX quick and dirty fix: chrome asks for favicon when opening report page
+        // which ends session without this check
+        if ("/favicon.ico".equals( pathInfo )) {
+            log.warn( "    ...skipped." );
+            return;
+        }
+        
         resp.sendRedirect( "polymap" );
     }
 
